4c0b133bd04a577d95b4b9dd8888a255812024fe
Developer <dev@example.com>
2026-06-23 15:10:46 +0800

    chore: install antd + router, remove LoginForm and unused assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import LoginForm from './components/LoginForm';
-
 function App() {
-  const handleLogin = (username: string, password: string) => {
-    console.log('登录信息:', { username, password });
-    alert('登录成功');
-  };
-
-  return <LoginForm onLogin={handleLogin} />;
+  return <div />;
 }
 
 export default App;
